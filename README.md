87b5b9415bf44d489c164f14202457a5b8d92036
Jason Soto <jsitech@live.com>
2015-09-16 20:23:21 -0500

    General Changes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SecureWPDeployer
 Automated Secure Wordpress Deployer - JackTheStripper + WPHardening
+================================================
+
+# SecureWPDeployer is a Bash Script that will automatically:
+
+* Deploy LAMP and Hardened your linux Server by Running the Deployer JackTheStripper for the Distro
+you Select, See  http://us.jsitech.com/jackthestripper or http://www.jsitech.com/jacktghestripper
+for more info.
+
+* Download and Install Wordpress on Selected Directory
+
+* Hardened the Wordpress Installation using WPHardening, see
+https://github.com/elcodigok/wphardening for more info
--- Secure_WP_Deployer.sh
+++ Secure_WP_Deployer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# SecureWPDeployer -Deployer Automatizado para el Despliegue Seguro de Wordpress
+# SecureWPDeployer - Automated Secure Wordpress Deployer
 #Jason Soto
+#jason_soto [AT] jsitech [DOT] com
 #www.jsitech.com
 #Twitter = @JsiTech
 
-# Securización del Servidor con JackTheStripper
-# Securización de Wordpress con WPHardening de Daniel Maldonado @elcodigok
+# Server Hardening With JackTheStripper
+# Wordpress Hardening With WPHardening From Daniel Maldonado @elcodigok
 
 
 # @license          http://www.gnu.org/licenses/gpl.txt  GNU GPL 3.0
@@ -13,70 +14,72 @@
 # @link             http://www.jsitech.com
 
 
-clear
-echo ""
-echo -e "\e[34m########################################################################\e[00m"
-echo ""
-echo -e "*** Automated Secure Wordpress Deployer - JackTheStripper + WPHardening ***"
-echo ""
-echo -e "\e[34m########################################################################\e[00m"
-echo ""
-sleep 3
-clear
+##############################################################################################################
+
+f_banner(){
+echo
+echo "
+ ____          __        __     ____             _
+/ ___|  ___  __\ \      / / __ |  _ \  ___ _ __ | | ___  _   _  ___ _ __
+\___ \ / _ \/ __\ \ /\ / / '_ \| | | |/ _ \ '_ \| |/ _ \| | | |/ _ \ '__|
+ ___) |  __/ (__ \ V  V /| |_) | |_| |  __/ |_) | | (_) | |_| |  __/ |
+|____/ \___|\___| \_/\_/ | .__/|____/ \___| .__/|_|\___/ \__, |\___|_|
+                         |_|              |_|            |___/
+
+By Jason Soto "
+echo
+echo
+}
+
+##############################################################################################################
 
 
-#Verificar si el Usuario es root
+#Check if running with root User
+
 if [ "$USER" != "root" ]; then
-      echo "Permiso Denegado"
-      echo "Solo puede ser ejecutado por root"
+      echo "Permission Denied"
+      echo "Can only be run by root"
       exit
 else
       clear
+      f_banner
       echo -e "\e[34m########################################################################\e[00m"
       echo ""
-      echo -e "     *** Bienvenidos al Deployer Automatizado de Wordpress***"
-      echo -e "Hardening del Server con JackTheStripper <www.jsitech.com/jackthestripper"
-      echo -e "Hardening de Wordpress con WPHardening <http://www.caceriadespammers.com.ar>"
+      echo -e "     *** Welcome to the Automated Secure Wordpress Deployer***"
+      echo -e "Server Hardening with JackTheStripper <www.jsitech.com/jackthestripper"
+      echo -e "Wordpress Hardening with WPHardening <http://www.caceriadespammers.com.ar>"
       echo ""
       echo -e "\e[34m########################################################################\e[00m"
       echo "  "
-fi 
+fi
 
-# Menu de Selección
+# Selection Menu
+clear
+f_banner
 echo ""
 sleep 2
 echo -e "\e[34m--------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[?]\e[00m Selecciona tu Distribución"
+echo -e "\e[93m[?]\e[00m Choose your Distro"
 echo -e "\e[34m--------------------------------------------------------------------------------------------\e[00m"
 echo ""
 echo " 1. Ubuntu_12.04"
-echo ""
 echo " 2. Ubuntu_14.04"
-echo ""
 echo " 3. Ubuntu_15.04"
-echo ""
 echo " 4. CentOS_6"
-echo ""
 echo " 5. CentOS_7"
-echo ""
 echo " 6. RHEL_6"
-echo ""
 echo " 7. RHEL_7"
-echo ""
 echo " 8. Debian_6"
-echo ""
 echo " 9. Debian_7"
-echo ""
 echo " 10. Debian_8"
-echo ""
-echo " 11. Salir"
+echo " 11. Exit"
 echo ""
 echo -e "\e[34m----------------------------------------------------------------------------------------------\e[00m"
 echo ""
 echo -ne "\e[93m>\e[00m "
 read options
 
-# Usando if se ejecuta la operación deseada
+# Using If to run the desired option
 if [ "$options" = "1" ]; then
     apt-get install bzr
     apt-get install python-git
@@ -104,11 +107,11 @@ elif [ "$options" = "3" ]; then
 elif [ "$options" = "4" ]; then
     yum install git
     yum install GitPython
-    git clone https://github.com/Jsitech/JTS-Centos-6     
+    git clone https://github.com/Jsitech/JTS-Centos-6
     cd JTS-Centos-6/
     chmod +x dms.sh
     ./dms.sh
-    
+
 elif [ "$options" = "5" ]; then
     yum install git
     yum install GitPython
@@ -163,53 +166,59 @@ elif [ "$options" = "10" ]; then
      ./dms.sh
 
 elif [ "$options" = "11" ]; then
-        echo " Seleccionó $options"
+        echo " You chose $options"
     exit
 
-# Si se elije otra opción repetir el menú
+# If another option is chosen repeat Menu
 else
     clear;
-    echo "Por favor seleccione una opción"
+    echo "Please select an option"
 
 fi
 
-#Continuar con la Instalación de Wordpress
-#Instalar CMS Wordpress
+#Proceed with Wordpress Installation
+#Install Wordpress CMS
+    clear
+    f_banner
     echo ""
-    echo -e "\e[93m[-]\e[00m Coloque un Nombre al Directorio que alojará Wordpress"
+    echo -e "\e[93m[-]\e[00m Name the Directory that will hold the Wordpress installation"
     echo ""
     echo -e "\e[34m--------------------------------------------------------------------\e[00m"
-    echo -e "\e[93m[?]\e[00m Como se llamará la Carpeta?"
+    echo -e "\e[93m[?]\e[00m Please type a name for the Directory : "
     echo -e "\e[34m--------------------------------------------------------------------\e[00m"
     echo ""
     echo -ne "\e[93m>\e[00m "
-    read CARPETA
+    read DIR
     echo ""
     echo ""
     wget http://wordpress.org/latest.tar.gz
     tar xzvf latest.tar.gz
-    mkdir /var/www/html/$CARPETA
-    cp -rf wordpress/* /var/www/html/$CARPETA/
+    mkdir /var/www/html/$DIR
+    cp -rf wordpress/* /var/www/html/$DIR/
 
 
-#Crear Base de Datos Para Wordpress
+#Create Wordpress Database
+clear
+f_banner
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[?]\e[00m Crearemos la Base de Datos para Wordpress"
+echo -e "\e[93m[?]\e[00m Going to Create the Wordpress Database"
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
 echo ""
-echo " *** ANOTE LOS DATOS QUE VA A INGRESAR AQUI, LO NECESITARA MAS ADELANTE ***"
-echo -n " Introduzca el Nombre de la Base de Datos: "; read db_name
-echo -n " Introduzca el Nombre de Usuario:  "; read db_user
-echo -n " Introduzca el Password para el Usuario:  "; read db_pass
+echo " *** MEMORIZE THE INFO YOU WILL TYPE HERE, WILL NEED IT LATER ***"
+echo -n " Type Database Name: "; read db_name
+echo -n " Type User:  "; read db_user
+echo -n " Type Password:  "; read db_pass
 cd ..
 chmod +x WPDBcreate.sh
 ./WPDBcreate.sh $db_name $db_user $db_pass
 echo ""
 echo ""
 
-#Asegurar Instalación de Wordpress con WPHardening de @elcodigok
+#Secure Wordpress Installation with WPHardening from @elcodigok
+clear
+f_banner
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[?]\e[00m Se Descargará WPHardening para Securizar esta instalación de Wordpress"
+echo -e "\e[93m[?]\e[00m We will now clone the WPHardening Repo tu Secure Wordpress"
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
 echo ""
 git clone https://github.com/elcodigok/wphardening.git
@@ -217,35 +226,36 @@ cd wphardening/
 echo ""
 sleep 2
 echo ""
-echo -e "\e[01;32m[-]\e[00m Asegurando Instalación de Wordpress"
+echo -e "\e[01;32m[-]\e[00m Securing Wordpress Installation"
 echo ""
-./wphardening.py -d /var/www/html/$CARPETA -v -c -r -f -t --wp-config --delete-version --robots --indexes --plugins
-mv /var/www/html/$CARPETA/wp-config-wphardening.php /var/www/html/$CARPETA/wp-config.php
+./wphardening.py -d /var/www/html/$DIR -v -c -r -f -t --wp-config --robots --indexes --plugins
+mv /var/www/html/$DIR/wp-config-wphardening.php /var/www/html/$DIR/wp-config.php
 
-#Determinar Base de Distro y Renombrar Directorio
+#Check Distro Base and Set Permissions
+clear
+f_banner
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[?]\e[00m Se Cambiara los permisos del Directorio de Wordpress"
+echo -e "\e[93m[?]\e[00m Setting Permissions on the Wordpress Directory"
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
 echo ""
 ls /usr/bin/dpkg > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    chown -R www-data:www-data /var/www/html/$CARPETA/
+    chown -R www-data:www-data /var/www/html/$DIR/
     sed -i s/SecRuleEngine\ On/SecRuleEngine\ DetectionOnly/g /etc/modsecurity/modsecurity.conf
 else
    rm -f /etc/httpd/conf.d/welcome.conf
-   chown -R apache:apache /var/www/html/$CARPETA/
+   chown -R apache:apache /var/www/html/$DIR/
    sed -i s/SecRuleEngine\ On/SecRuleEngine\ DetectionOnly/g /etc/httpd/conf.d/mod_security.conf
 fi
 
+clear
+f_banner
 echo ""
 sleep 2
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[?]\e[00m Despliegue Finalizado, Se procederá a Reiniciar el Servidor"
+echo -e "\e[93m[?]\e[00m Deployment finished, Will reboot Server"
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[?]\e[00m Luego de reiniciado deberá acceder por SSH via el Puerto 372 si es Ubuntu o Debian O 2020 si Es Red Hat o Centos"
+echo -e "\e[93m[?]\e[00m After Restarting you can access your Server Remotely via port 372 for Debian Based or 2020 for Red Hat Based Distros"
 echo ""
 sleep 10
 reboot
-
-
-
